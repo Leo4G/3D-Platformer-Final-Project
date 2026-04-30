de32d10b4e36fc95f72af049646f1b3a79b05026
leo.gordy26@pallottihs.info <leo.gordy26@pallottihs.info>
2026-04-30 09:44:41 -0400

    Coin progress

---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PlayerData : MonoBehaviour
+{
+  // public int coins;
+
+
+
+    //void OnTriggerEnter3D(Collider3D other)
+   // {
+       // if (other.gameObject.tag == "Coin")
+       // {
+          //  coins += 1;
+            
+           // other.gameObject.SetActive(false)
+       // }
+    
+   // }
+}
